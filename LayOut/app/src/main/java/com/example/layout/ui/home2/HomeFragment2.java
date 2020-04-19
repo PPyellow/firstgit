83bf877fa88ea3b32d52110de09ef68e8c4c5c39
YpYp <hycsun1996a@gmail.com>
2020-04-19 12:08:24 +0800

    first submission

---
+++ LayOut/app/src/main/java/com/example/layout/ui/home2/HomeFragment2.java
@@ -0,0 +1,38 @@
+package com.example.layout.ui.home2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.layout.R;
+
+public class HomeFragment2 extends Fragment {
+
+    private HomeFragment2ViewModel mViewModel;
+
+    public static HomeFragment2 newInstance() {
+        return new HomeFragment2();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home2, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeFragment2ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ LayOut/app/src/main/java/com/example/layout/ui/home2/HomeFragment2ViewModel.java
@@ -0,0 +1,7 @@
+package com.example.layout.ui.home2;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeFragment2ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
